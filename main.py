e7283e10d4a31b5ca8f84c8c539a9078f300f221
Soe Wei Yan Phyo <38011380+soeweiyan-phyo@users.noreply.github.com>
2024-01-31 21:39:07 +1100

    Add download video functionality

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 
+# To download video from YouTube
+def start_download():
+  try:
+    yt_link = link.get()
+    ytObject = YouTube(yt_link)
+    video = ytObject.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    video.download()
+  except:
+    print("YouTube link is invalid")
+  print("Download completed")
+
 # System Settings
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
@@ -21,7 +32,8 @@ link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
 link.pack()
 
 # Download Button
-download = customtkinter.CTkButton(app, text="Download", command=startDownload)
+download = customtkinter.CTkButton(app, text="Download", command=start_download)
+download.pack(padx=10, pady=10)
 
 # Run App 
 # (Have to loop, otherwise app closes immediately)
