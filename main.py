7a3dbc70a5bd77c9cd8e5cc45815c0155e1fc946
Soe Wei Yan Phyo <38011380+soeweiyan-phyo@users.noreply.github.com>
2024-01-31 20:51:57 +1100

    Add ui elements

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ app = customtkinter.CTk()
 app.geometry("720x480")
 app.title("Youtube Downloader")
 
+# Adding UI Elements
+title = customtkinter.CTkLabel(app, text="Insert a youtube link")
+title.pack(padx=10, pady=10)
+
+# Link Input
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
+link.pack()
+
+# Download Button
+download = customtkinter.CTkButton(app, text="Download", command=startDownload)
+
 # Run App 
 # (Have to loop, otherwise app closes immediately)
 app.mainloop()
