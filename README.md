f655bf8b0e539b3d063233e05b9ac3ea27d98e48
Soe Wei Yan Phyo <38011380+soeweiyan-phyo@users.noreply.github.com>
2024-01-31 23:06:15 +1100

    Add progress percentage and bar

---
+++ README.md
@@ -0,0 +1,34 @@
+# YouTube Video Downloader (GUI)
+
+This a GUI app to download YouTube video by entering the video link.
+
+### Usage Instructions:
+
+- **Prerequisites:** Install **pytube** and **customtkinter**.
+- Run the code: `python main.py`
+- Or run here: https://replit.com/@soeweiyanphyo/Youtube-Downloader-GUI
+
+### Features:
+
+- GUI
+- Download video
+- Progress percentage and bar
+
+### Technology Stack:
+
+- **Language:** Python
+- **Libraries:** pytube | tkinter | customtkinter
+
+### Credits and Acknowledgments:
+
+- Build A Modern Python GUI Project | Step by Step Tutorial [https://youtu.be/NI9LXzo0UY0]
+- pytube Documentation [https://pytube.io/en/latest/index.html]
+
+### Further Improvements:
+
+- Dropdown menu to select video quality
+- Download audio only
+- To view and rename the download filename
+- To view and change the download path
+- To show the download speed
+- Improve UI layout and design
--- main.py
+++ main.py
@@ -2,21 +2,41 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 
-# To download video from YouTube
+# Download video from YouTube
 def start_download():
   try:
+    # Get link from link UI input
     yt_link = link.get()
-    ytObject = YouTube(yt_link)
+    # Create YouTube object
+    ytObject = YouTube(yt_link, on_progress_callback=on_progress)
+    # Get video file from the YouTube object
     video = ytObject.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
 
+    # Change title to video title
     title.configure(text=ytObject.title, text_color="black")
     finish_label.configure(text="", text_color="black")
     
+    # Download video
     video.download()
     finish_label.configure(text="Download Completed")
 
   except:
     finish_label.configure(text="Download Error", text_color="red")
+
+# Progress calculation callback function for YouTube object
+def on_progress(stream, chunk, bytes_remaining):
+  # Calculate completion percentage
+  total_size = stream.filesize
+  bytes_downloaded = total_size - bytes_remaining
+  completion_percent = bytes_downloaded / total_size * 100
+  percent = str(int(completion_percent))
+
+  # Update progress percentage UI
+  progress_percent.configure(text=percent + "%")
+  progress_percent.update() # Update on every interation
+  
+  # Update progress bar UI
+  progress_bar.set(float(completion_percent) / 100)
 
 # System Settings
 customtkinter.set_appearance_mode("System")
@@ -29,7 +49,7 @@ app.title("Youtube Downloader")
 
 # Adding UI Elements
 title = customtkinter.CTkLabel(app, text="Insert a youtube link")
-title.pack(padx=10, pady=10)
+title.pack(padx=10, pady=10) # Show the UI element
 
 # Link Input
 url_var = tkinter.StringVar()
@@ -39,6 +59,14 @@ link.pack()
 # Finished Downloading
 finish_label = customtkinter.CTkLabel(app, text="")
 finish_label.pack()
+
+# Progress percentage
+progress_percent = customtkinter.CTkLabel(app, text="0%")
+progress_percent.pack()
+
+progress_bar = customtkinter.CTkProgressBar(app, width=400)
+progress_bar.set(0)
+progress_bar.pack(padx=10, pady=10)
 
 # Download Button
 download = customtkinter.CTkButton(app, text="Download", command=start_download)
